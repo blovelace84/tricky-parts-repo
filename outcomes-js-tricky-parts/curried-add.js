e6f2f89f0c29a96a447ac1e940a32cb169d92ea3
blovelace84 <blovelace84@gmail.com>
2024-09-21 08:17:49 -0400

    Update all

--- outcomes-js-tricky-parts/curried-add.js
+++ outcomes-js-tricky-parts/curried-add.js
@@ -1,5 +1,10 @@
 function curriedAdd(total) {
-
+    if(total === undefinded) return 0;
+    return function addNext(num){
+        if(num === undefinded) return total;
+        total += num;
+        return addNext;
+    };
 }
 
 module.exports = { curriedAdd };
